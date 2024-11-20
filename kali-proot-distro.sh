e9976d2049bc855703e00e1e495a7f5426d838a0
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-21 07:50:57 +0800

    Update kali-proot-distro.sh

--- kali-proot-distro.sh
+++ kali-proot-distro.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 ###
-# echo "PS1='\${debian_chroot:+(\$debian_chroot)}\u@kali:\w\$ '" | tee -a /etc/bash.bashrc > /dev/null
-wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali-nethunter/refs/heads/main/bash.bashrc
+wget -O /root/.bashrc https://raw.githubusercontent.com/xiv3r/proot-distro-kali-nethunter/refs/heads/main/.bashrc
 ###
 apt update && apt install curl gnupg -y
 ###
